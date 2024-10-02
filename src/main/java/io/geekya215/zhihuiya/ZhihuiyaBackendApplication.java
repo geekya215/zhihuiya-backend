f5b99300ff3cd64186cec918158fd370f5512050
Lin Yang <geekya215@gmail.com>
2024-10-02 23:41:19 +0800

    initial springboot project template

---
+++ src/main/java/io/geekya215/zhihuiya/ZhihuiyaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.geekya215.zhihuiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuiyaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuiyaBackendApplication.class, args);
+    }
+
+}
